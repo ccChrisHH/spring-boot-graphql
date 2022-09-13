bd6232ceaa213eae0a10f673759d3fb44b29a727
Christian Zyweck <christian.zyweck@otto.de>
2022-09-13 09:54:58 +0200

    raw project setup

---
+++ src/main/kotlin/de/codecentric/tutorials/springbootgraphql/SpringBootGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package de.codecentric.tutorials.springbootgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootGraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/de/codecentric/tutorials/springbootgraphql/SpringBootGraphqlApplicationTests.kt
@@ -0,0 +1,12 @@
+package de.codecentric.tutorials.springbootgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootGraphqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
